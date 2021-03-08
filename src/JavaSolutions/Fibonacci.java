4ecb92e00fca87a2cc2326485bbb4ccb3db0d15f
yusuf <yusuf@kucukvatan.com>
2021-03-08 08:32:44 -0500

    change-2

--- src/JavaSolutions/Fibonacci.java
+++ src/JavaSolutions/Fibonacci.java
@@ -1,19 +1,22 @@
 package JavaSolutions;
 
+import java.util.ArrayList;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        fibonacci(10);
+        System.out.println(fibonacci(5));
     }
-    static int n1=1, n2=2, n3=0;
 
-    public static void fibonacci(int count){
+    static int n1=1, n2=2, n3=0;
+    static ArrayList<Integer> fiboList = new ArrayList<>();
+    public static ArrayList<Integer> fibonacci(int count){
         if (count > 0) {
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
-            System.out.print(" " + n3);
+            fiboList.add(n3);
             fibonacci(count-1);
         }
-
+        return fiboList;
     }
 }
